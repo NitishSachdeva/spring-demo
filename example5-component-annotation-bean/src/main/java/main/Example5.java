2c235479531bf9d29b0044e162bcfca242864070
NitishSachdeva <nitishsachdeva39@gmail.com>
2022-09-13 02:21:18 +0530

    component annotation

---
+++ example5-component-annotation-bean/src/main/java/main/Example5.java
@@ -0,0 +1,17 @@
+package main;
+
+import beans.Vehicle;
+import config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example5 {
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        System.out.println("Component Vehicle name from Spring Context is: " + vehicle.getName());
+        vehicle.printHello();
+
+    }
+}
