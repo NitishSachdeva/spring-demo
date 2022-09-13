556f552268f15f889ed484693db23dc569516d7e
NitishSachdeva <nitishsachdeva39@gmail.com>
2022-09-13 23:51:09 +0530

    creating beans with register bean

---
+++ example7-creating-new-bean-with-registerbean/src/main/java/beans/Vehicle.java
@@ -0,0 +1,24 @@
+package beans;
+
+import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component
+public class Vehicle {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printHello(){
+        System.out.println(
+                "Printing Hello from Component Vehicle Bean");
+    }
+}
